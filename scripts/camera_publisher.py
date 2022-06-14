5d2c45c2d7ae27948f2a27cfacd3ca3074af8d99
Mohammad Abadi <mohammad.abadi@conigital.com>
2022-06-14 17:31:59 +0100

    trace the code and clear the unused comments

--- scripts/camera_publisher.py
+++ scripts/camera_publisher.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
+
 import rospy
 import cv2
-import numpy as np
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-
 
 # Node to obtain call camera data. Separate I/O pipeline
 rospy.loginfo('Init Cameras...')
@@ -12,6 +10,7 @@ cam_front = cv2.VideoCapture(0)
 cam_front.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cam_front.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 cam_front.set(cv2.CAP_PROP_FOURCC, int(0x47504A4D))
+
 
 def cv2_to_imgmsg(cv_image):
     img_msg = Image()
@@ -23,23 +22,15 @@ def cv2_to_imgmsg(cv_image):
     img_msg.step = len(img_msg.data) // img_msg.height # That double line is actually integer division, not a comment
     return img_msg
 
+
 def imagePublisher():
+
     front_pub = rospy.Publisher('/center_cam', Image, queue_size=1)
     rospy.init_node('camera_publisher', anonymous=True)
-    #rate=rospy.Rate(30)#10hz
-    bridge = CvBridge()
 
     while not rospy.is_shutdown():
         _, front_img = cam_front.read()
-
-        # cv2.imshow('image', front_img)        # for debugging purposes
         front_img = cv2_to_imgmsg(front_img)
-        # rospy.loginfo("images sent")
-        # for debugging purposes, remove if cv2.imshow('imgae', img) is deleted
-        k = cv2.waitKey(1) & 0xFF
-        if k ==27:
-            break
-
         front_pub.publish(front_img)
 
     cv2.destroyAllWindows()
@@ -49,6 +40,5 @@ def imagePublisher():
 if __name__ == '__main__':
     try:
         imagePublisher()
-        # print('sending image')
     except rospy.ROSInterruptException:
         pass
